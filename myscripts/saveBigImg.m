47d94eda19da256b3999a003bc1d3317037a6fdc
POWER-Morzh <denys.korzh@gmail.com>
2014-06-14 19:29:29 +0200

    Finish of saveBigImg.m

--- myscripts/saveBigImg.m
+++ myscripts/saveBigImg.m
@@ -1,13 +1,13 @@
 clear all;
 
-%figPNG = figure; % this figure is used to print plots in PNG files
+figPNG = figure('units','normalized','outerposition',[0 0 1 1]); % this figure is used to print plots in PNG files
 
 imgPath = '../img/'; % folder in wich the program will save all PNG
 filePath = '../output/'; % folder with dat files
 
 FACTOR = 2.^[0:0];
 MANTISSA = 7:8;
-FACTOR = [0.5 FACTOR];
+%FACTOR = [0.5 FACTOR];
 
 filenamelist = char('coordinateGlobalL2ErrorNorm', ...
 	'coordinate_global_max_error', ...
@@ -15,28 +15,37 @@ filenamelist = char('coordinateGlobalL2ErrorNorm', ...
     'globalL2ErrorNorm', ...
     'global_max_error', ...
     'globalMaxL2ErrorNorm');
-%filenamelist = char('coordinateGlobalL2ErrorNorm');
+% filenamelist = char('coordinateGlobalL2ErrorNorm');
+
+% for i = 1:size (filenamelist,1)
+%     for f_i = 1:length(FACTOR)
+%         filename = [deblank(filenamelist(i,:))];
+%         hfigSurf(f_i,i) = figure( 'units','normalized','outerposition',[0 0 1 1],...
+%             'Name',[filename '-FACTOR-' num2str(FACTOR(f_i))] );% Creation of figures for different times
+%     end
+% end
+
 
 for i = 1:size (filenamelist,1)
     for f_i = 1:length(FACTOR)
-        filename = [deblank(filenamelist(i,:))];
-        hfigSurf(f_i,i) = figure( 'Name',[filename '-FACTOR-' num2str(FACTOR(f_i))] );% Creation of figures for different times
-    end
-end
-
-
-for f_i = 1:length(FACTOR)
-    for m_i = 1:length(MANTISSA)
-        for i = 1:size (filenamelist,1)
-            %img = figure(figPNG);
-
-            filename = [deblank(filenamelist(i,:)) '-F' num2str(FACTOR(f_i)) '-M' num2str(MANTISSA(m_i))];
+        savefilename = [ deblank(filenamelist(i,:)) '-F' num2str(FACTOR(f_i)) ];
+        clf; % Very important, is it's not done then many plots will be on one graph
+        for m_i = 1:length(MANTISSA)
+            filename = [ savefilename '-M' num2str(MANTISSA(m_i)) ];
 
             globalNorm = load([filePath filename '.dat']);
             
-            figure(hfigSurf(f_i,i)); % Create figure which will be divided in 4 parts
+            
+            %figure(hfigSurf(f_i,i)); % Create figure which will be divided in 4 parts
+            img = figure(figPNG);
                 subplot(2,2,m_i);
+                hold on;
+                xlabel(['MANTISSA='  num2str(MANTISSA(m_i))]);
                 plot( globalNorm(:,1) );
+                hold off;
+                
+            imgName = [savefilename '.png'];
+            print(img, '-dpng', [imgPath imgName]);
 
             
         end
